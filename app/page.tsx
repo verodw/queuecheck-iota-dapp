36e18c45d90a58f97ee5bc612f9f50c33f60c4d3
verodw <veronicadwiyanti11@gmail.com>
2025-12-15 16:08:43 +0700

    fix page import to use queuecheck

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { WalletConnect } from "@/components/Wallet-connect"
-import SampleIntegration from "@/components/sample"
+import QueueCheck from "@/components/QueueCheck"
 
 export default function Home() {
   return (
-    <div>
-      {/* <WalletConnect /> */}
-      <SampleIntegration />
-    </div>
+    <main>
+      <QueueCheck />
+    </main>
   )
 }
